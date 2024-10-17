c8416c41549bf34cf5fcd2e1d0b2e7952c7858c5
H1nkk <the_h1nk@mail.ru>
2024-10-17 18:24:02 +0300

    Changed sample_matrix file

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -10,22 +10,41 @@
 
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
   setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  TDynamicMatrix<int> d(c);
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+  int sz;
+  cout << "Введите размер матриц: ";
+  cin >> sz;
+
+  TDynamicMatrix<int> a(sz), b(sz);
+  cout << "Введите первую матрицу:\n";
+  cin >> a;
+  cout << "Введите вторую матрицу:\n";
+  cin >> b;
+
+  char op;
+  cout << "Введите операцию, производимую над матрицами (1 символ +,-,*): ";
+  cin >> op;
+
+  TDynamicMatrix<int> c;
+  switch (op)
+  {
+  case '+':
+	  c = a + b;
+	  cout << "Сумма введенных матриц:";
+	  cout << endl << c << endl;
+	  break;
+  case '*':
+	  c = a * b;
+	  cout << "Произведение введенных матриц:";
+	  cout << endl << c << endl;
+	  break;
+  case '-':
+	  c = a - b;
+	  cout << "Разность матриц a и b:";
+	  cout << endl << c << endl;
+	  break;
+  default:
+	  break;
+  }
 }
 //---------------------------------------------------------------------------
